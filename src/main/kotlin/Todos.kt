aeac08725b460b3aa3c27422311d75bbfc2ce025
Duncan McGregor <duncan@oneeyedmen.com>
2024-04-15 17:11:30 +0100

    Implement deleteItemById method in Todos class
    This commit adds the deleteItemById function to the Todos class which allows items to be removed from the list by their unique ID. A corresponding test has been provided on the TodosTest file to ensure the functionality of this new method. The Todos class also has been updated

--- src/main/kotlin/Todos.kt
+++ src/main/kotlin/Todos.kt
@@ -1,16 +1,16 @@
 import java.util.UUID
 
 class Todos(
-    items: List<TodoItem>
+    private val items: MutableList<TodoItem>
 ) : List<TodoItem> by items {
+
     fun deleteItemById(uuid: UUID) {
-
+        items.removeIf { it.id == uuid }
     }
-
 }
 
 fun Todos(items: List<String>) = Todos(
-    items.map { name -> TodoItem(UUID.randomUUID(), name) }
+    items.map { name -> TodoItem(UUID.randomUUID(), name) }.toMutableList()
 )
 
 data class TodoItem(
--- src/test/kotlin/TodosTests.kt
+++ src/test/kotlin/TodosTests.kt
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.doesNotContain
 import strikt.assertions.isEqualTo
+import strikt.assertions.size
 import java.util.*
 
 
@@ -12,5 +14,15 @@ class TodosTests {
         val notThere = UUID.randomUUID()
         items.deleteItemById(notThere)
         expectThat(items.size).isEqualTo(3)
+    }
+
+    @Test
+    fun `deleteItemById deletes`() {
+        val second = items[1]
+        items.deleteItemById(second.id)
+        expectThat(items) {
+            size.isEqualTo(2)
+            doesNotContain(second)
+        }
     }
 }
