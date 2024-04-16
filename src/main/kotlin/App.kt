f135269694d510b1a9480238b30032c01c869cac
Duncan McGregor <duncan@oneeyedmen.com>
2024-04-16 09:18:44 +0100

    Implement DELETE route and method for Todos
    The DELETE HTTP method has been added to the application by implementing the `deleteHandler` and linked it to the "/item/{id}" route in the Todos class. Moreover, a test for this new feature has been provided in the AppTest file, ensuring that `deleteItemById` function properly removes an item from the Todos list.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,14 @@
 import org.http4k.core.*
+import org.http4k.filter.DebuggingFilters
+import org.http4k.filter.ServerFilters
+import org.http4k.lens.BiDiPathLens
+import org.http4k.lens.Path
+import org.http4k.lens.value
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.util.UUID
 
 fun main() {
     println("Something happened!!")
@@ -22,8 +28,23 @@ val Todos.listHandler: HttpHandler
         )
     }
 
+val idLens = Path.of("id")
+
+val Todos.deleteHandler: HttpHandler
+    get() = {
+        val id = UUID.fromString(idLens(it))
+        this.deleteItemById(id)
+        Response(Status.OK)
+    }
+
 val Todos.routes: HttpHandler
-    get() = routes(
-        "/" bind Method.GET to rootHandler,
-        "/items" bind Method.GET to this.listHandler
-    )
+    get() = ServerFilters
+        .CatchAll().then(ServerFilters.CatchLensFailure())
+        .then(DebuggingFilters.PrintRequestAndResponse())
+        .then(
+            routes(
+                "/" bind Method.GET to rootHandler,
+                "/items" bind Method.GET to this.listHandler,
+                "/item/{id}" bind Method.DELETE to this.deleteHandler
+            )
+        )
--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -1,7 +1,4 @@
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.*
 import org.http4k.strikt.bodyString
 import org.http4k.strikt.status
 import org.http4k.testing.ApprovalTest
@@ -9,7 +6,9 @@ import org.http4k.testing.Approver
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import strikt.api.expectThat
+import strikt.assertions.doesNotContain
 import strikt.assertions.isEqualTo
+import strikt.assertions.size
 
 @ExtendWith(ApprovalTest::class)
 class AppTest {
@@ -34,6 +33,14 @@ class AppTest {
 
     @Test
     fun deleteItemFromToDo() {
-       items.routes(Request(Method.DELETE, "/items"))
+        val second = items[1]
+        val response = items.routes(Request(Method.DELETE, UriTemplate.from("/item/{id}")).with(idLens of second.id.toString()))
+        expectThat(response) {
+            status.isEqualTo(Status.OK)
+        }
+        expectThat(items) {
+            size.isEqualTo(2)
+            doesNotContain(second)
+        }
     }
 }
