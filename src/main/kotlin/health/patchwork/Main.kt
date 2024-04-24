f4139efbc17e00f1c4ab522ad2033526b6771c20
Radosław Trochimiuk <radoslaw.trochimiuk97@gmail.com>
2024-04-24 13:26:14 +0200

    Split handler

--- src/main/kotlin/health/patchwork/Main.kt
+++ src/main/kotlin/health/patchwork/Main.kt
@@ -1,6 +1,5 @@
 package health.patchwork
 
-import org.http4k.core.HttpHandler
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
@@ -8,11 +7,14 @@ import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
 fun main() {
-    handler.asServer(Undertow(8080)).start()
+    val toDos: List<Item> = listOf(Item("Buy Milk"), Item("Buy Coffee"))
+    toDos.handler().asServer(Undertow(8080)).start()
 }
 
-val handler : HttpHandler = { request -> Response(Status.OK).body("hello") }
-
 fun List<Item>.handler(): (request: Request) -> Response {
-    return { Response(Status.OK).body(this.joinToString("\n") { it.name }) }
+    return { request ->
+        if (request.uri.path == "/")
+            Response(Status.OK).body("hello")
+        else
+            Response(Status.OK).body(this.joinToString("\n") { it.name }) }
 }
--- src/test/kotlin/health/patchwork/Tests.kt
+++ src/test/kotlin/health/patchwork/Tests.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Tests {
+    val toDos: List<Item> = listOf(Item("Buy Milk"), Item("Buy Coffee"))
+    val handler: HttpHandler = toDos.handler()
     @Test
     fun test() {
         println("Hello world!")
@@ -14,13 +16,11 @@ class Tests {
     fun testHandler(){
         val response = handler(Request(Method.GET, "/"))
         assertEquals(Status.OK, response.status)
+        assertEquals("hello", response.bodyString())
     }
 
     @Test
     fun listToDos() {
-        val toDos: List<Item> = listOf(Item("Buy Milk"), Item("Buy Coffee"))
-        val handler: HttpHandler = toDos.handler()
-
         assertEquals(
             Response(Status.OK).body("Buy Milk\nBuy Coffee"),
             handler(Request(Method.GET, "/listToDos"))
