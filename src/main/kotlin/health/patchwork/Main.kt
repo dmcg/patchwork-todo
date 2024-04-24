d8d63159f065ff4e46fe2339072cb15ff3c73fa7
Radosław Trochimiuk <radoslaw.trochimiuk97@gmail.com>
2024-04-24 12:58:53 +0200

    toDo handler

--- src/main/kotlin/health/patchwork/Main.kt
+++ src/main/kotlin/health/patchwork/Main.kt
@@ -1,6 +1,7 @@
 package health.patchwork
 
 import org.http4k.core.HttpHandler
+import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.server.Undertow
@@ -11,3 +12,7 @@ fun main() {
 }
 
 val handler : HttpHandler = { request -> Response(Status.OK).body("hello") }
+
+fun List<Item>.handler(): (request: Request) -> Response {
+    return { Response(Status.OK).body(this.joinToString("\n") { it.name }) }
+}
