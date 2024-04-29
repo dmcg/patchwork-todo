0f7cd0c28aaff1ffc66905d669e9d65f11976f79
Duncan McGregor <duncan@oneeyedmen.com>
2024-04-29 14:51:09 +0100

    Add POST method to HTTP handler and tests
    The HTTP handler now supports POST requests. This will allow adding new ToDo items to the list. Associated unit tests were also updated to test this new functionality. A new test was added to verify the functionality of the newly added POST request support.

--- src/main/kotlin/com/patchwork/main.kt
+++ src/main/kotlin/com/patchwork/main.kt
@@ -1,22 +1,29 @@
 package com.patchwork
 
 import org.http4k.core.HttpHandler
+import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
 fun main() {
-    val items = emptyList<ToDoItem>()
+    val items = mutableListOf<ToDoItem>()
     items.toHandler().asServer(Undertow(port = 8080)).start()
 }
 
-fun List<ToDoItem>.toHandler(): HttpHandler {
-    return {
-        Response(Status.OK).body(joinToString("\n") { it.name })
+fun MutableList<ToDoItem>.toHandler(): HttpHandler {
+    return { request ->
+        when (request.method) {
+            Method.GET -> Response(Status.OK).body(joinToString("\n") { it.name })
+            Method.POST -> {
+                this.add(ToDoItem(request.bodyString()))
+                Response(Status.CREATED)
+            }
+
+            else -> TODO()
+        }
     }
 }
 
-data class ToDoItem(val name: String) {
-
-}
+data class ToDoItem(val name: String)
--- src/test/kotlin/com/patchwork/TodoTests.kt
+++ src/test/kotlin/com/patchwork/TodoTests.kt
@@ -1,6 +1,5 @@
 package com.patchwork
 
-import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Status
@@ -8,15 +7,16 @@ import org.http4k.strikt.bodyString
 import org.http4k.strikt.status
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
 
 class TodoTests {
-    val items = mutableListOf<ToDoItem>()
-    val client: HttpHandler = items.toHandler()
+    private val items = mutableListOf<ToDoItem>()
+    private val client = items.toHandler()
 
     @Test
     fun `returns empty body for no items`() {
-        expectThat(client(Request(Method.GET, "http://localhost:8080/"))) {
+        expectThat(client(Request(Method.GET, "/items"))) {
             status.isEqualTo(Status.OK)
             bodyString.isEqualTo("")
         }
@@ -25,9 +25,17 @@ class TodoTests {
     @Test
     fun `returns multiple items in body`() {
         items.addAll(listOf(ToDoItem("Buy milk"), ToDoItem("Buy bread")))
-        expectThat(client(Request(Method.GET, "http://localhost:8080/"))) {
+        expectThat(client(Request(Method.GET, "/items"))) {
             status.isEqualTo(Status.OK)
             bodyString.isEqualTo("Buy milk\nBuy bread")
         }
+    }
+
+    @Test
+    fun `can add an item`(){
+        expectThat(client(Request(Method.POST, "/items").body("Buy beer"))) {
+            status.isEqualTo(Status.CREATED)
+        }
+        expectThat(items).hasSize(1).and { get { first().name }.isEqualTo("Buy beer")  }
     }
 }
